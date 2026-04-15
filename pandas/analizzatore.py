cb41ea70bc36150fd0f7699d05e91a199ad513d8
maboglia <mauro.bogliaccino@gmail.com>
2026-04-15 13:47:53 +0200

    aggiornamento

--- pandas/analizzatore.py
+++ pandas/analizzatore.py
@@ -4,22 +4,28 @@ import sqlite3
 connessione = sqlite3.connect("auto_modificato.db")
 
 df = pd.read_csv("auto.csv")
+df_moto = pd.read_csv("moto.csv")
 
 print(df.head())
 print(df.tail(3))
 print(df.columns)
 print(df[["Marca", "Modello"]])
 
-df = df[["Marca", "Modello"]]
+df = pd.concat([df, df_moto], ignore_index=True)
 
-df.to_csv("auto_modificato.csv", index=False)
-
-df.to_json("auto_modificato.json", orient="records", lines=True)
-
-df.to_excel("auto_modificato.xlsx", index=False)
-
-df.to_html("auto_modificato.html", index=False)
+print(df.groupby("Marca")["Marca"].count())
 
 
+# df = df[["Marca", "Modello"]]
 
-df.to_sql("auto_modificato.db", index=False, if_exists="replace", con=connessione)
+# df.to_csv("auto_modificato.csv", index=False)
+
+# df.to_json("auto_modificato.json", orient="records", lines=True)
+
+# df.to_excel("auto_modificato.xlsx", index=False)
+
+# df.to_html("auto_modificato.html", index=False)
+
+
+
+# df.to_sql("auto_modificato.db", index=False, if_exists="replace", con=connessione)
