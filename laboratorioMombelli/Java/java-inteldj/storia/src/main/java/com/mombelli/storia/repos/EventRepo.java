d9187982905269a3632edbcfdf3a4819ba39eba7
maboglia <mauro.bogliaccino@gmail.com>
2026-07-17 11:32:25 +0200

    debug storia mombellica

--- laboratorioMombelli/Java/java-inteldj/storia/src/main/java/com/mombelli/storia/repos/EventRepo.java
+++ laboratorioMombelli/Java/java-inteldj/storia/src/main/java/com/mombelli/storia/repos/EventRepo.java
@@ -3,5 +3,5 @@ package com.mombelli.storia.repos;
 import com.mombelli.storia.entities.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EventRepo extends JpaRepository<Event, Integer> {
+public interface EventRepo extends JpaRepository<Event, Long> {
 }
