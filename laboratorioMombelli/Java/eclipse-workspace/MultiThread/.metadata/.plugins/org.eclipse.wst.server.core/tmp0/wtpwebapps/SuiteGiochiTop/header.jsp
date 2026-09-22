749a590fab23f81c78f6eaf1ef94700a3dde4377
Maikol Mombelli <maikol.mombelli@edu-its.it>
2026-09-22 12:22:27 +0200

    Lezione del 22/09/2026

---
+++ laboratorioMombelli/Java/eclipse-workspace/MultiThread/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/SuiteGiochiTop/header.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<div class="container" >
+	
+	<nav>
+  <ul>
+    <li><strong>Acme Corp</strong></li>
+  </ul>
+  <ul>
+    <li><a href="">Home</a></li>
+    <li><a href="?pagina=memory">Memory</a></li>
+    <li><a href="?pagina=wordle">Wordle</a></li>
+    <li><a href="?pagina=hangman">Impiccato</a></li>
+    <li><a href="#">Products</a></li>
+  </ul>
+</nav>
+	
+
+
+
---
+++ laboratorioMombelli/Java/eclipse-workspace/MultiThread/MultiThreading/src/lez01/Main.java
@@ -0,0 +1,33 @@
+package lez01;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		String name = Thread.currentThread().getName();
+		System.out.println("Il nome del thread: " + name);
+		System.out.println("######## Race start#######");
+		
+		RaceThread eclipse = new RaceThread("Eclipse", 1000);
+		RaceThread pitone = new RaceThread("Python", 2000);
+		
+		eclipse.start();
+		pitone.start();
+		
+		try {
+			eclipse.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			pitone.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("####### Race stop########");
+	}
+
+}
